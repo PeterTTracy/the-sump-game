1415b6649858ae51fffcf13c5b7eb500413e34d0
deploy <deploy@local>
2026-06-20 17:33:31 -0400

    Deploy The Sump PWA

---
+++ assets/web-2e4m3JJd.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-BoHDmEbz.js";import"./vendor-phaser-CaWnzXme.js";class i extends n{async show(e){}async hide(e){}}export{i as SplashScreenWeb};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/the-sump-game/sw.js', { scope: '/the-sump-game/' })})}
